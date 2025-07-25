4c7770bdce348418dbeb09c06dede8c556b01c62
Naina Subherwal <nainasubherwal@gmail.com>
2025-07-25 13:12:53 -0700

    Create and set up NewToDoView

---
+++ ToDoList/NewToDoView.swift
@@ -0,0 +1,24 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    
+    var body: some View {
+        
+        
+        
+    }//body
+    
+}//struct
+
+#Preview {
+    
+    NewToDoView()
+    
+}//preview
